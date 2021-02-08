ec8231a5ada2ec48843f9f985d5e22abd2f2d91e
Robert Møller <robert.moeller@booza.app>
2021-02-08 15:02:46 +0100

    * Added markdown parsing * Added workflow on master push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
-import { MainComponent } from './layout/main/main.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './layout/header/header.component';
+import {MainComponent} from './layout/main/main.component';
+import {MarkdownModule, MarkdownService} from "ngx-markdown";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,9 +14,12 @@ import { MainComponent } from './layout/main/main.component';
     MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({loader: HttpClient})
   ],
-  providers: [],
+  providers: [MarkdownService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
